c620ad7eda235e7cbde73e7de3545357fb5e292f
Emmanuel <emmanu-0000@hotmail.com>
2019-03-22 00:50:40 -0500

    Agregando primera interactividad

--- src/renderer/frontend.js
+++ src/renderer/frontend.js
@@ -1,3 +1,33 @@
-document.addEventListener('load',() => {
-  
-})
+const desactivarBoton =()=> document.getElementById('tabla1').style.display = 'none'
+desactivarBoton()
+
+// function desactivarTablas() {
+//   document.getElementById('tabla1').style.display = 'none'
+//   document.getElementById('tabla2').style.display = 'none'
+//   document.getElementById('tabla3').style.display = 'none'
+// }
+//
+// const botones = ['.btn1','.btn2','.btn3','.btn4']
+// // var botonActual = botones.map((valor)=>document.getElementById(valor))
+//
+// switch (botones) {
+//   case 1:
+//       if (document.querySelector(botones[0])){
+//         desactivarTablas()
+//         document.getElementById('tabla1').style.display = 'block'
+//       }else {
+//         document.getElementById('btn1').style.display = 'block'
+//       }
+//     break;
+//   case 2:
+//
+//     break;
+//   case 3:
+//
+//     break;
+//   case 4:
+//
+//     break;
+//   default:
+//
+// }
